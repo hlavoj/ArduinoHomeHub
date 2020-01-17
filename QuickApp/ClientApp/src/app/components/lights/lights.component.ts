d4e85f60af2bf685bd0e535f9130f20571058a26
Vojtech Hlavačka <hlavoj@gmail.com>
2020-01-17 15:31:18 +0100

    lights table

--- QuickApp/ClientApp/src/app/components/lights/lights.component.ts
+++ QuickApp/ClientApp/src/app/components/lights/lights.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit,TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { LightsEndpointService } from '../../services/http/lights-endpoint.service'
 import { Light } from '../../models/light.model';
+import { AlertService, DialogType, MessageSeverity } from '../../services/alert.service';
+import { AppTranslationService } from '../../services/app-translation.service';
+import { Utilities } from '../../services/utilities';
 
 @Component({
   selector: 'app-lights',
@@ -8,60 +11,92 @@ import { Light } from '../../models/light.model';
   styleUrls: ['./lights.component.css']
 })
 export class LightsComponent implements OnInit {
+  columns: any[] = [];
+  rows: Light[] = [];
+  rowsCache: Light[] = [];
+  editedUser: Light;
+  sourceUser: Light;
+  editingUserName: { name: string };
+  loadingIndicator: boolean;
+  //reorderable = true;
 
-  
+
   @ViewChild('colorTemplate', { static: true })
   colorTemplate: TemplateRef<any>;
-  
+
   @ViewChild('turnOnTemplate', { static: true })
   turnOnTemplate: TemplateRef<any>;
 
-  loadingIndicator = false;
-  reorderable = true;
-  rows = [
-    // { id: 5, turnOn: 'Male', intensity: 'Swimlane', color: 'ddd' }
-    // { name: 'Dany', gender: 'Male', company: 'KFC' },
-    // { name: 'Molly', gender: 'Female', company: 'Burger King' },
-  ];
-  columns = [
-    { prop: 'id' , name : 'neco'},
-    { name: 'turnOn' , cellTemplate: this.turnOnTemplate },
-    { name: 'intensity' },
-    { prop: 'color', name: 'Color' , cellTemplate: this.colorTemplate }
-  ];
+  @ViewChild('indexTemplate', { static: true })
+  indexTemplate: TemplateRef<any>;
+
+  @ViewChild('actionsTemplate', { static: true })
+  actionsTemplate: TemplateRef<any>;
 
   constructor(
+    private alertService: AlertService, private translationService: AppTranslationService,
     private lightsService: LightsEndpointService
   ) { }
 
   ngOnInit() {
-    this.lightsService.getLights().subscribe(lights => 
-      this.proccessLights(lights))
 
-      this.columns = [
-        { prop: 'id' , name : 'neco'},
-        { name: 'turnOn' , cellTemplate: this.turnOnTemplate},
-        { name: 'intensity' },
-        { prop: 'color', name: 'Color' , cellTemplate: this.colorTemplate }
-      ];
+    //const gT = (key: string) => this.translationService.getTranslation(key);
+
+    this.columns = [
+      { prop: 'id', name: 'Id' },
+      { prop: 'turnOn', name: 'Turn On', cellTemplate: this.turnOnTemplate },
+      { prop: 'intensity', name: 'Intensity' },
+      { prop: 'color', name: 'Color', cellTemplate: this.colorTemplate },
+      { name: '', width: 240, cellTemplate: this.actionsTemplate, resizeable: false, canAutoResize: false, sortable: false, draggable: false }
+    ];
+
+    this.loadData();
+  }
+
+
+  loadData() {
+    this.alertService.startLoadingMessage();
+    this.loadingIndicator = true;
+
+    this.lightsService.getLights().subscribe(lights => this.proccessLights(lights), error => this.onDataLoadFailed(error))
   }
 
   proccessLights(lights: Light[]): void {
-    console.info('test 3', lights);
+    this.alertService.stopLoadingMessage();
+    this.loadingIndicator = false;
+
+    lights.forEach((light, index, users) => {
+      (light as any).index = index + 1;
+    });
+
+    this.rowsCache = [...lights];
 
     this.rows = lights;
   }
 
-  turnOnLite(row){
-    console.info('turn on lite', row);
+  onDataLoadFailed(error: any) {
+    this.alertService.stopLoadingMessage();
+    this.loadingIndicator = false;
+
+    this.alertService.showStickyMessage('Load Error', `Unable to retrieve users from the server.\r\nErrors: "${Utilities.getHttpResponseMessages(error)}"`,
+      MessageSeverity.error, error);
   }
 
-  edit(row){
-    console.info('edit lite', row);
+
+  turnOnLite(row) {
+    console.info('turn on light', row);
   }
 
-  delete(row){
-    console.info('delete lite', row);
+  edit(row) {
+    console.info('edit light', row);
+  }
+
+  delete(row) {
+    console.info('delete light', row);
+  }
+
+  detail(row) {
+    console.info('detail light', row);
   }
 
 }
