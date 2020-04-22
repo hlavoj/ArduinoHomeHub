d5b9a1a65ddaa27da69489c21e81535bc68db02c
Vojtech Hlavačka <hlavoj@gmail.com>
2020-04-22 15:46:04 +0200

    backend to https

--- QuickApp/ClientApp/src/environments/environment.prod.ts
+++ QuickApp/ClientApp/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
     production: true,
-    baseUrl: 'http://arduinohomehubbe.azurewebsites.net', // Change this to the address of your backend API if different from frontend address
+    baseUrl: 'https://arduinohomehubbe.azurewebsites.net', // Change this to the address of your backend API if different from frontend address
     tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
     loginUrl: '/login'
 };
